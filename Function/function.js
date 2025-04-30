65a52f30b8b7a09a21be09c80520f5b86488ecfe
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-04-30 23:24:42 +0530

    commit Function

---
+++ Function/function.js
@@ -0,0 +1,56 @@
+const a = 20;
+const b = 20;
+
+
+
+
+function add(x,y){
+    
+    const add = x + y;
+   // console.log(add);
+
+}
+add(a,b);
+
+
+
+
+
+
+function sub(x,y){
+    
+    //console.log(x-y);
+
+}
+sub(a,b);
+
+
+
+
+
+
+
+
+function mul(x,y){
+    
+    const mul = x * y;
+    return mul;
+
+}
+const return_mul = mul(a,b);
+//console.log(return_mul);
+
+
+
+
+
+function divi(x,y){
+     
+    return x / y;
+
+}
+const return_divi = divi(a,b);
+//console.log(return_divi);
+
+
+
