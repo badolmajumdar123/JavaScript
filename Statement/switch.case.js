67d0970520a80bcb475191e88b0efbad681297aa
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-01 02:00:36 +0530

    commit switch-case

---
+++ Statement/switch.case.js
@@ -0,0 +1,235 @@
+
+//1.....
+
+const option = 4;
+
+
+switch (option) {
+    case 1:
+        console.log("Welcome");
+        break;
+         
+        case 2:
+            console.log("Hello");
+            break;
+
+            case 3:
+               console.log("Hi");
+               break;
+                  
+               case 4:
+                console.log("Ok");
+                break;
+    default:
+        break;
+
+
+
+    };
+
+
+
+
+//2.......
+
+const Oparetor = "/";
+const a = 2;
+const b = 2;
+
+
+   switch (Oparetor) {
+    case "+":
+        let add = a + b;
+        console.log(add);
+        break;
+       
+       
+           case "-":
+                let sub = a - b;
+                console.log(sub);
+                break;
+                    
+
+                case "*":
+                    let mul = a * b;
+                    console.log(mul);
+                    break;
+
+                      
+                    case "/":
+                         let divi = a / b;
+                         console.log(divi);
+                         break;
+    default:
+        break;
+   
+   
+    };
+
+
+
+
+
+
+//3......
+
+
+
+
+const number = 6;
+
+
+  switch (number) {
+    case 1:
+        
+         let num1 = 20 + 20;
+         console.log(num1);
+
+        break;
+             
+
+        case 2:
+
+            let num2 = 20 - 20;
+            console.log(num2);
+
+           break;
+
+          
+           case 3:
+
+              let num3 = 20 * 20;
+              console.log(num3);
+
+              break;
+                 
+              case 4:
+
+                 let num4 = 20 / 20;
+                 console.log(num4);
+
+                 break;
+                   
+
+                   case 5:
+
+                       let num5 = 20 + 30;
+                       console.log(num5);
+
+                       break;  
+                         
+
+                       case 6:
+
+                          let num6 = 30 + 30;
+                          console.log(num6);
+
+                           break;
+    default:
+        break;
+  
+  
+    };
+
+
+
+
+
+
+//4.....
+
+
+
+
+
+
+  const value = "value3";
+
+
+
+    
+
+  switch (value) {
+    case "value1":
+        console.log("WELCOME");
+        break;
+        
+        case "value2":
+            console.log("OK");
+            break;
+              
+            case "value3":
+                console.log("GOOD");
+                break;
+    default:
+        break;
+  
+  
+  
+  
+    };
+
+
+
+
+
+//5........
+
+
+
+ const name = "Joy";
+
+
+         switch (name) {
+            case "Badol":
+
+                console.log("badol@gmail.com");
+                console.log(700141270);
+
+                break;
+
+
+                   
+                case "Shomrat":
+
+                    console.log("shomrat@gmail.com");
+                    console.log(700141271);
+
+                    break;
+
+                        
+                    case "Avi":
+
+                        console.log("avi@gmail.com");
+                        console.log(700141272);
+
+                        break;
+                              
+                          
+                        case "Monty":
+
+                            console.log("monty@gmail.com");
+                            console.log(700141273)
+
+                            break;
+
+
+                            case "Chandhan":
+
+                                console.log("chandhan@gmail.com");
+                                console.log(700141274);
+
+                                break;
+                                 
+
+                                case "Joy":
+
+                                    console.log("joy@gmail.com");
+                                    console.log(700141275);
+
+                                    break;
+
+                          
+            default:
+                break;
+         }
