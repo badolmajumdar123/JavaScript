d1fd62c4c951b5d87718c990646b8b52953aa267
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-01 00:34:43 +0530

    commit if-else-statement

---
+++ Statement/if.else.statement.js
@@ -0,0 +1,124 @@
+//
+
+
+const age1 = 20;
+
+
+if(age1 >= 18){
+
+     // console.log("you can vote");
+
+}else{
+
+    // console.log("you can not vote");
+
+};
+
+
+
+
+//
+
+
+
+
+
+const age = 2334;
+
+
+if(age <= 5 && age > 0){
+
+   // console.log("baby");
+
+}else if(age < 18 && age > 5){
+
+    // console.log("child");
+
+}else if (age >= 18 && age <= 100){
+
+   // console.log("adult");
+
+
+}else{
+    
+  //console.log("not found");
+
+};
+
+
+
+
+//
+
+
+
+
+
+
+
+const result = 90;
+
+
+
+if(result >= 33 && result < 40){
+
+    // console.log("D");
+
+}else if(result >= 40 && result < 50){
+
+    // console.log("C");
+
+}else if(result >= 50 && result < 60){
+
+      //console.log("B");
+ 
+}else if(result >= 60 && result < 70){
+
+      //console.log("A-");  
+
+}else if(result >= 70 && result < 80){
+     
+      // console.log("A");
+
+}else if(result >= 80 && result <= 100){
+
+       // console.log("A+");
+
+}else{
+
+       //console.log("Fail");
+
+};
+
+
+
+
+
+
+//
+
+
+
+
+const mobile = "7001412701"
+
+const email = "badol@gmail.com"
+
+
+if(mobile === "7001412701" || email === "badol@gmail.com"){
+
+   //console.log(true);
+
+}else{
+
+   //console.log(false);
+
+};
+
+
+
+
+
+
+
+
