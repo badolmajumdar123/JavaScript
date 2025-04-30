eb7afcd42d6655f4445517d4a2dfa1ed23e367af
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-01 02:26:33 +0530

    commit For-Loop

---
+++ Loop/for.loop.js
@@ -0,0 +1,80 @@
+
+//1.......
+
+//console.log("Badol");
+
+
+
+for(let i = 1; i <= 5; i++){
+
+   // console.log("Badol");
+
+};
+
+
+
+//2.......
+
+for(let i = 1; i <= 10; i++){
+
+const add = 20 + 20;
+//console.log(add);
+
+
+};
+
+
+
+//3.......
+
+
+const a = 20;
+const b = 20;
+
+const mul = a * b;
+
+
+for(let i = 1; i <= 15; i++){
+
+//console.log(mul);
+
+};
+
+
+
+
+//4.........
+
+
+
+for(let i = 1; i <= 5; i++){
+
+    //console.log("WELCOME TO MY PROFILE");
+
+};
+
+
+
+
+//5.........
+
+
+
+for(let i = 1; i <= 10; i++){
+
+  console.log("WELCOME");
+  console.log("HELLO");
+  console.log("ANYTHING")
+
+
+}
+
+
+
+
+
+
+
+
+
+
