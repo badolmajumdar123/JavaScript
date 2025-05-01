bb6471abbefdd0fad486ed545a534fac4bb86414
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-02 01:03:29 +0530

    commit do-while-loop

---
+++ Loop/do.while.loop.js
@@ -0,0 +1,51 @@
+//1........
+
+let i = 1;
+
+do {
+
+   // console.log("WELCOME");
+    i++
+    
+} while (i <= 5);
+
+
+
+
+//2........
+
+
+let j = 1;
+const a = 12;
+const b = 12;
+
+
+do {
+    
+    const mul = a * b;
+    //console.log(mul);
+    j++
+    
+
+} while (j <= 10);
+
+
+
+
+
+
+//3.........
+
+
+let v = 1;
+
+do {
+
+    const x = 40;
+    const y = 40;
+    const add = x + y;
+    //console.log(add);
+    v++
+
+    
+} while (v <= 15);
