2193a85e68846a4d15d9da41033685fc75cf1208
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-02 17:58:38 +0530

    commit practice-loop

---
+++ Loop/practice.loop.js
@@ -0,0 +1,108 @@
+//The odd number from 1 to 100;
+
+
+for(let i = 1; i <= 100; i = i + 2){
+
+    let odd_Number = i;
+   // console.log(odd_Number);
+
+};
+
+
+
+
+//The even number from 1 to 100;
+
+
+
+for(let i = 2; i <= 100; i = i + 2){
+
+    let even_Number = i;
+    //console.log(even_Number);
+
+};
+
+
+
+
+//Sum of number from 0 to 5;
+
+let sum = 0;
+
+for(let i = 0; i <= 5; i++){
+
+    sum = sum + i;
+   //console.log(sum)
+    
+    
+};
+
+
+
+
+// Sum of odd number from 1 to 100;
+
+let sum1 = 0;
+
+for(let i = 1; i <= 100; i = i + 2){
+
+    sum1 = sum1 + i;
+    //console.log(sum1);
+
+};
+
+
+
+
+//sum of even number from 1 to 100;
+
+
+let sum2 = 0;
+
+for(let i = 2; i <= 100; i = i + 2){
+
+   sum2 = sum2 + i;
+ //console.log(sum2)
+
+};
+
+
+
+
+// five of Multiplication Table;
+
+
+let num1 = 5;
+
+
+for(let i = 1; i <= 10; i++){
+
+let five_Multiplication = `${num1} * ${i} = ${num1*i}`;
+//console.log(five_Multiplication);
+//console.log(num1 * i);
+
+};
+
+
+
+
+
+
+//one to ten of Multiplication Table;
+
+
+for(let i = 1; i <= 10; i++){
+  
+    
+     
+    for(let v = 1; v <= 10; v++){
+
+          let OneToTen_Multiplication = `${v} * ${i} = ${v*i}`;
+        // console.log(OneToTen_Multiplication)
+         //console.log(i*v);
+    };
+
+
+};
+
+
